c9142200bab3dbf39646b22893653613e50a292b
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-14 18:29:17 +0300

    Add files via upload

--- hmwrk_1.3.js
+++ hmwrk_1.3.js
@@ -1,6 +1,7 @@
 function run(){
   var a = prompt("Enter number A:");
   var b = prompt("Enter number B:");
-
+  b = [a, a = b][0];
+  console.log("a: "+a+" b: "+b);
 }
 run();
---
+++ hmwrk_1.4.js
@@ -0,0 +1,11 @@
+function run(){
+  var a = prompt("Enter number A:");
+  var b = prompt("Enter number B:");
+  if (a>b) {
+    b = [a, a = b][0];
+    console.log("a: "+a+" b: "+b);
+  } else {
+    console.log("a: "+a+" b: "+b);
+  }
+}
+run();
