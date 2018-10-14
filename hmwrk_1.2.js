adee64533eac6bce60333a4a095b33ba339b6bfc
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-14 14:35:24 +0300

    Add files via upload

---
+++ hmwrk_1.2.js
@@ -0,0 +1,19 @@
+function run(){
+var a = prompt("Enter number A:");
+var b = prompt("Enter number B:");
+
+var addition = a + b;
+console.log("Addition: "+addition);
+
+var subtraction = a - b;
+console.log("Substraction: " + subtraction);
+
+var multiplication = a*b;
+console.log("Multiplication: " + multiplication);
+
+var division = a/b;
+console.log("Division: "+division);
+
+
+}
+run();
