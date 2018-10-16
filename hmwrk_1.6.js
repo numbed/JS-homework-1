b487f6a4ab776b4071859a6a960ace192469edce
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-16 10:22:07 +0300

    Update hmwrk_1.6.js

--- hmwrk_1.6.js
+++ hmwrk_1.6.js
@@ -2,10 +2,8 @@ function run(){
   var a1 = prompt("Enter number a1:");
   var a2 = prompt("Enter number a2:");
   var a3 = prompt("Enter number a3:");
-  [a1, a2, a3] = [1, 2, 3];
-  console.log(a+" "+a2+ " " + a3);
-  [a2, a3, a1] = [a, a2, a3];
-  console.log(a+" "+a2+ " " + a3);
-
+  console.log(a1 + " " + a2 + " " + a3);
+  [a2, a3, a1] = [a1, a2, a3];
+  console.log(a1 + " " + a2 + " " + a3);
 }
 run();
