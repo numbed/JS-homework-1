d9e4cc937801f9e8bc572fcd7652bfbf31d869b2
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-16 16:00:32 +0300

    Add files via upload

---
+++ hmwrk_1.7.js
@@ -0,0 +1,24 @@
+function run() {
+	var hour = prompt("Enter hour:\nonly whole numbers (7,14,22)");
+	var cash = prompt("Enter cash:");
+	var health = confirm("Are you healthy?\nOK for YES | CANCEL for NO");
+	hour = parseInt(hour);
+	cash = parseFloat(cash);
+	if (health == true) {
+		if (hour > 22 && cash > 100) {
+			console.log("I'm ready for party. Let's get wasted!");
+		} else {
+			console.log("Loneliness awaits me!");
+		}
+		
+	} else {
+
+		console.log("I will stay at home.");
+		if (cash > 50) {
+			console.log("I will buy medicine.");
+		} else {
+			console.log("Herbal tea, here I come!");
+		}
+	}
+}
+run();
